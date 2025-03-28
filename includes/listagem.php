51846df5e10db68edca5f58a2719b79153d6111d
ALUNO <caiofelipesmoreira@gmail.com>
2025-03-28 16:05:18 -0300

    Atualizando listagem

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,4 +1,16 @@
 <?php
+
+    $mensagem = '';
+    if(isset($_GET['status'])) {
+        switch ($_GET['status']) {
+            case 'sucess':
+                $mensagem = '<div class="alert alert-success">Ação executada com sucesso!</div>';
+                break;
+            case 'error':
+                $mensagem = '<div class="alert alert-danger">Ação não executada!</div>';
+        }
+    }
+
     $resultados = '';
     foreach($vagas as $vaga) {
         $resultados .= '<tr>
@@ -17,8 +29,18 @@
                             </td>
                         </tr>';
     }
+
+    $resultados = strlen($resultados) ? $resultados : ' <tr>
+                                                            <td colspan="6" class="text-center">
+                                                                <strong>Nenhuma vaga encontrada.</strong>
+                                                            </td>
+                                                        </tr>';
+
 ?>
 <main>
+
+    <?=$mensagem?>
+
     <selection>
         <a href="cadastrar.php">
             <button class="btn btn-success">Nova vaga</button>
